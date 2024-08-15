29140e5e43acaad4391bcac64a9191c47513fb34
João Vitor Oliveira <97929873+JoaoOliveira02@users.noreply.github.com>
2024-08-15 10:53:19 -0300

    Sistema NunesStorage
    Arquivos do sistema, para funcionamento. Um CRUD básico.

---
+++ Models/Produtos.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NunesStorage.Models;
+
+public class Produtos
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Nome { get; set; }
+
+    [Required]
+    public string Codigo { get; set; }
+
+    public string? Descricao { get; set; }
+
+    [Required]
+    public Decimal Preco { get; set; }
+ 
+}
